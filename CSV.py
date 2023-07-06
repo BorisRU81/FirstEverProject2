474679088a4031e76c6b6ad756cc154c3964dd30
Boris <Hrom80@bk.ru>
2023-07-06 16:13:17 +0300

    Создаем и читаем на нескольких примерах

--- CSV.py
+++ CSV.py
@@ -14,15 +14,42 @@ import csv
 #         print(x, y, z)
 
 
-goods = [('Ковер', 5000), ('телевизор', 3000), ('Кресло', 1500), ('Стол', 2500), ('Холодильник', 4850)]
-with open('goods.csv', 'w', newline='', encoding="utf-8") as f:
-    writer = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for i in goods:
-        writer.writerow(i)
-        #print(i)
+# goods = [('Ковер', 5000), ('телевизор', 3000), ('Кресло', 1500), ('Стол', 2500), ('Холодильник', 4850)]
+# with open('goods.csv', 'w', newline='', encoding="utf-8") as f:
+#     writer = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#     for i in goods:
+#         writer.writerow(i)
+#         #print(i)
+#
+# with open('goods.csv', 'r', encoding='utf-8') as f:
+#      reader = csv.reader(f, delimiter=';', quotechar='"')
+#      for i in reader:
+#         x, y = i
+#         #print(x, y)
 
-with open('goods.csv', 'r', encoding='utf-8') as f:
-     reader = csv.reader(f, delimiter=';', quotechar='"')
-     for i in reader:
-        x, y = i
-        #print(x, y)
+
+data = [{
+    'lastname': 'Салем',
+    'firstname': 'Борис',
+    'class_number': 9,
+    'class_letter': 'А'
+}, {
+    'lastname': 'Пушкин',
+    'firstname': 'Виталик',
+    'class_number': 9,
+    'class_letter': 'Б'
+}]
+with open('form.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames=list(data[0].keys()),
+                            delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+    writer.writeheader()
+    for item in data:
+        writer.writerow(item)
+
+
+with open('form.csv', 'r', encoding='utf-8') as f:
+      reader = csv.DictReader(f, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)
+      #header = next(reader)
+      for item in reader:
+          for x, y in item.items():
+              print(x, y)
